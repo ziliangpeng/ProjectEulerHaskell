46214266696b39b87079f991a338410288332c94
Victor Peng <ziliangdotme@gmail.com>
2013-11-08 02:59:19 +1030

    shorter and more minimal problem 2

--- 2.hs
+++ 2.hs
@@ -1,12 +1,5 @@
-fib :: (Integral a) => a -> a
-fib 1 = 1
-fib 2 = 1
-fib x = fib (x-1) + fib (x-2)
+fib = 1:1:(map s (zip fib (tail fib))) where s (x,y) = x + y
 
-fibSeq = [fib x | x <- [1..35]] 
+answer = sum([x | x <- takeWhile (<bound) fib, x `mod` 2 == 0]) where bound = 4000000
 
-answer = sum([x | x <- fibSeq, x <= 4000000, x `mod` 2 == 0])
-
-ansStr = show answer
-
-main = putStrLn ansStr
+main = print answer

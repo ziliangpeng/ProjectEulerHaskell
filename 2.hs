bbb7c18c3dc7ce527826dc80697c096199f893f2
Victor Peng <ziliangdotme@gmail.com>
2013-11-11 22:25:06 +1030

    tweak 2.hs

--- 2.hs
+++ 2.hs
@@ -1,5 +1,7 @@
-fib = 1:1:(map s (zip fib (tail fib))) where s (x,y) = x + y
+fib = 1:1:(map (\(x,y) -> x + y) (zip fib (tail fib))) where s (x,y) = x + y
 
-answer = sum([x | x <- takeWhile (<bound) fib, x `mod` 2 == 0]) where bound = 4000000
+bound = 4000000
+
+answer = sum([x | x <- takeWhile (<bound) fib, x `mod` 2 == 0])
 
 main = print answer
